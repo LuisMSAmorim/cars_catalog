e93858d323a02f2817f8ae8c6ab88eb355f301bf
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2022-04-26 13:35:59 -0300

    Add presence validations to car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,8 @@
 class Car < ApplicationRecord
+
+    validates :name, presence: true
+    validates :brand, presence: true
+    validates :color, presence: true
+    validates :year, presence: true
+    validates :price, presence: true
 end
