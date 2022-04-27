e25d7ab373d6993321f193e37a3e0a10e4045ebb
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2022-04-27 13:56:13 -0300

    Add car unit model tests

---
+++ spec/unit/models/car_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Car, type: :model do
+
+    describe "validations" do
+
+        describe 'title' do
+            it { is_expected.to validate_presence_of(:model) }
+        end
+
+        describe 'brand' do
+            it { is_expected.to validate_presence_of(:brand) }
+        end
+
+        describe 'color' do
+            it { is_expected.to validate_presence_of(:color) }
+        end
+
+        describe 'year' do
+            it { is_expected.to validate_presence_of(:value) }
+        end
+    end
+end
